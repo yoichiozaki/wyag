9737509950b2261818ee45450eec7b97c813f736
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-20 13:29:26 +0000

    🚀 wyag init

--- libwyag.py
+++ libwyag.py
@@ -11,6 +11,10 @@ argparser = argparse.ArgumentParser(description="The stupid content tracker")
 
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
+argsp = argsubparsers.add_parser(
+    "init", help="Initialize a new, empty repository.")
+argsp.add_argument("path", metavar="directory", nargs="?",
+                   default=".", help="Where to create the repository.")
 
 
 def main(argv=sys.argv[1:]):
@@ -44,3 +48,134 @@ def main(argv=sys.argv[1:]):
         cmd_show_ref(args)
     elif args.command == "tag":
         cmd_tag(args)
+
+
+class GitRepository(object):
+    """A git repository"""
+
+    worktree = None
+    gitdir = None  # .git
+    conf = None  # .git/config
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git repository {}".format(path))
+
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            # core.repositoryformatversion
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception(
+                    "Unsupported repositoryformatversion {}".format(vers))
+
+
+def repo_path(repo, *path):
+    """Compute path under repo's gitdir"""
+    return os.path.join(repo.gitdir, *path)
+
+
+def repo_file(repo, *path, mkdir=False):
+    """Same as repo_path, but create dirname(*path) if absent. For example, repo_file(r, \"refs\", \"remotes\", \" origin\". \"HEAD\") will create .git/refs/remotes/origin."""
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir."""
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory {}".format(path))
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+
+def repo_create(path):
+    """Create a new repository at path"""
+    repo = GitRepository(path, force=True)
+
+    # First, we make sure that the path either does not exists or is an empty dir.
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception("{} is not a directory!".format(path))
+        if os.listdir(repo.worktree):
+            raise Exception("{} is not empty".format(path))
+    else:
+        os.makedirs(repo.worktree)
+
+    assert(repo_dir(repo, "branches", mkdir=True))  # .git/branches/
+    assert(repo_dir(repo, "objects", mkdir=True))  # .git/objects/
+    assert(repo_dir(repo, "refs", "tags", mkdir=True))  # .git/refs/tags/
+    assert(repo_dir(repo, "refs", "heads", mkdir=True))  # .git/refs/heads/
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write(
+            "Unnamed repository: edit this file 'description' to name the repository.\n")
+
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/main\n")
+
+    # .git/config
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
+
+
+def repo_find(path=".", required=True):
+    path = os.path.realpath(path)
+
+    if os.path.isdir(os.path.join(path, ".git")):
+        return GitRepository(path)
+
+    # If we have not returned, recurse in parent.
+    parent = os.path.realpath(os.path.join(path, ".."))
+
+    if parent == path:  # base case
+        if required:
+            raise Exception("No git directory.")
+        else:
+            return None
+
+    return repo_find(parent, required=required)
+
+#############################################################
+# wyag init
+# usage: wyag init <path>
+#############################################################
+
+
+def cmd_init(args):
+    repo_create(args.path)
