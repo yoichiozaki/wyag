457a15fb8771c2e7374c5ed78bfa1a2891e07e71
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-21 06:00:43 +0000

    update object_find

--- libwyag.py
+++ libwyag.py
@@ -313,8 +313,71 @@ def object_write(obj, actually_write=True):
     return sha
 
 
+def object_resolve(repo, name):
+    """Resolve name to an object hash in repo.
+    This function is aware of:
+    - the HEAD literal
+    - short and long hashes
+    - tags
+    - branches
+    - remote branches"""
+
+    candidates = list()
+    hashRE = re.compile(r"^[0-9A-Fa-f]{1,16}$")
+    smallHashRE = re.compile(r"^[0-9A-Fa-f]{1,16}$")
+
+    if not name.strip():
+        return None
+
+    if name == "HEAD":
+        return [ref_resolve(repo, "HEAD")]
+
+    if hashRE.match(name):
+        if len(name) == 40:
+            return [name.lower()]
+        elif 4 <= len(name):
+            name = name.lower()
+            prefix = name[0:2]
+            path = repo_dir(repo, "objects", prefix, mkdir=False)
+            if path:
+                rem = name[2:]
+                for f in os.listdir(path):
+                    if f.startswith(rem):
+                        candidates.append(prefix + f)
+    return candidates
+
+
 def object_find(repo, name, fmt=None, follow=True):
-    return name
+    sha = object_resolve(repo, name)
+
+    if not sha:
+        raise Exception("No such reference {0}.".format(name))
+
+    if len(sha) > 1:
+        raise Exception(
+            "Ambiguous reference {0}: Candidates are:\n - {1}.".format(name,  "\n - ".join(sha)))
+
+    sha = sha[0]
+
+    if not fmt:
+        return sha
+
+    while True:
+        obj = object_read(repo, sha)
+
+        if obj.fmt == fmt:
+            return sha
+
+        if not follow:
+            return None
+
+        # Follow tags
+        if obj.fmt == b'tag':
+            sha = obj.kvlm[b'object'].decode("ascii")
+        elif obj.fmt == b'commit' and fmt == b'tree':
+            sha = obj.kvlm[b'tree'].decode("ascii")
+        else:
+            return None
 
 
 def kvlm_parse(raw, start=0, dct=None):
